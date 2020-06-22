d96f5ab504f65ae3cba2e3856c66869c8844c4a7
Jorg van der Ham <jorgvanderham@Jorgs-MacBook-Pro.local>
2020-06-22 16:36:19 +0200

    fixed env var

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 class AuthService {
   constructor() {
     this.service = axios.create({
-      baseURL: 'http://localhost:4000',
+      baseURL: process.env.REACT_APP_BASE_URL,
       withCredentials: true
     })
     //this.service = service
